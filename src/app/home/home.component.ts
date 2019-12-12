8c9744c8fb84d1ff0e17396b0227fd1fce19c10b
Vanessa Conceição <vanessa@itecnologia.com.br>
2019-12-12 14:47:07 -0300

    Ofertas Service with Ofertas class

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../shared/services/ofertas.service';
+import { Oferta } from '../shared/models/ofertas.model';
 
 @Component({
   selector: 'pu-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[]
+
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas()
+    console.log(this.ofertas)
   }
 
 }
